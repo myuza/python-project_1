c78fd59f14fa4aebc7b17503113b7a5e7e26f97c
18010533 <muhammad_y_ansor_x1@telkomsel.co.id>
2023-04-04 22:19:23 +0700

    final editing

--- dags/main.py
+++ dags/main.py
@@ -1,91 +1,119 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.operators.python_operator import PythonOperator
-from datetime import datetime
+from airflow.operators.dummy import DummyOperator
+from airflow.sensors.python import PythonSensor
+import datetime
+import pandas as pd, glob, os, airflow.utils.dates, sys
 from pathlib import Path
-import pandas, json, glob
-
-default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2023,4,2),
-    'depends_on_past': False,
-    'retries': 0
-}
-
 dag = DAG(
-    'product_stock_synch',
-    default_args = default_args,
-    schedule_interval = None
+    dag_id="student_project_1",
+    start_date=airflow.utils.dates.days_ago(7),
+    schedule_interval= None,
+    description="Student Project IT Product Catalogue",
 )
-data_path = "/home/airflow/data"
-data_file = glob.glob(str(data_path+"/data.csv"), recursive=True)
-df = pandas.read_csv(data_file, names=("sku","name","stock"), skiprows = 1)
-result = df.to_json(orient='records')
-parsed = json.loads(result)
+path    = "/home/airflow/data"
+dateTime = datetime.datetime.now()     
+def buat_file():
+    directory = Path(path)
+    directory.mkdir(parents=True, exist_ok=True)
+    data1 = {
+            "sku": ["TM1", "TM2", "TM3", "TM4", "TM5", "TM6"],
+            "name": ["Digimax", "MaxStream", "IndieHome", "Halo", "UltraData", "MaxData"],
+            "stock": [15, 13, -13, 25, 12, 17],
+    }
+    data2 = {
+            "sku": ["TM1", "TM2", "TM3", "TM4", "TM5", "TM6"],
+            "name": ["Digimax", "MaxStream", "IndieHome", "Halo", "UltraData", "MaxData"],
+            "stock": [13, 15, 14, 11, 8, 15],
+    }
+    df1 = pd.DataFrame(data1)
+    df2 = pd.DataFrame(data2)
 
-# parsed = [
-#     {"sku":"A1","name":"pants","stock":-3},
-#     {"sku":"A5","name":"kaos","stock":5},
-#     {"sku":"A4","name":"topi","stock":3},
-#     ]
+    df1.to_csv(str(path+'/product_'+dateTime.strftime("%Y%m%d%H%M")+'_1.csv'), index=False)
+    df2.to_csv(str(path+'/product_'+dateTime.strftime("%Y%m%d%H%M")+'_2.csv'), index=False)
 
-def calculateStock(oldStock, newStock):
-    return 0 if oldStock + newStock < 0 else oldStock + newStock
+    print("Memulai membuat file csv")
 
-def updateStock(data):
+def proses_data():
+    allFiles = glob.glob(str(path+"/*.csv"), recursive=True)
 
-    pg_hook = PostgresHook(
-        postgres_conn_id='local_postgres',
-        schema='test'
-    )
-    pg_conn = pg_hook.get_conn()
-    cursor = pg_conn.cursor()
+    for files in allFiles:
+        # Open Postgres Connection
+        conn = PostgresHook(postgres_conn_id='postgre_airflow').get_conn()
+        cursor = conn.cursor()
+        with open(files, 'r') as f:
+            df = pd.read_csv(f)
+            print("Proses input data dari csv")
+            for index, row in df.iterrows():                
+                cursor.execute('select count(*) from product where sku = %s', (row['sku'],))
+                result = cursor.fetchone()
 
-    for p in data:
-        sku = p['sku']
-        name = p['name']
+                for res in result:
+                    print(res)
+                print("Total number of rows on sku "+row['sku']+" : ", res)
+                
+                # sys.exit()
+                if res < 1:
+                    params = (row['sku'], row['name'], row['stock'])
 
-        sql_stmt = "select * from products where sku = '" + sku + "'"
-        cursor.execute(sql_stmt)
-        fetch_data = cursor.fetchone()
+                    cursor.execute(
+                        "INSERT INTO product (sku, name, stock) VALUES (%s, %s, %s)", params
+                    )
+                else:
+                    params = (row['stock'], row['sku'])
 
-        if(fetch_data is None):
-            stock = 0 if p['stock'] < 0 else p['stock']
-            sql_stmt = "INSERT INTO products (sku,name,stock) VALUES  ('"+ p['sku'] + "','" + p['name'] + "',"+ str(stock) +")"
-        else :
-            stock = calculateStock(fetch_data[2], p['stock'])
-            sql_stmt = "UPDATE products SET name = '"+ name +"', stock="+ str(stock) + "where sku = '"+ sku +"'"
+                    cursor.execute(
+                        "UPDATE product set stock = %s where sku = %s", params
+                    )            
+            conn.commit()
+            # cursor.copy_from(f, 'product', sep=',')
+            # conn.commit()
+            # cursor.close()
+def hapus_file():
+    allFiles = glob.glob(str(path+"/*.csv"), recursive=True)
+    for files in allFiles:
+        os.remove(files)
+        print("Deleting File. . .")
 
-        cursor.execute(sql_stmt)
-        pg_conn.commit()
-    
-    cursor.close()
-    pg_conn.close()
-
-update_stock = PythonOperator(
-    task_id="update_stock",
-    python_callable=updateStock,
-    op_kwargs={'data': parsed},
+buatTable = PostgresOperator(
+    task_id = 'buat_table',
+    postgres_conn_id = 'postgre_airflow',
+    sql = '''
+         create table if not exists product(
+            id SERIAL PRIMARY KEY,
+            sku VARCHAR NOT NULL,
+            name VARCHAR NOT NULL,
+            stock INTEGER NOT NULL not null default 0
+        );
+    ''',
     dag = dag
 )
 
-delete_file = BashOperator(
-    task_id = 'delete_file',
-    bash_command = 'rm $(ls -t /home/airflow/data/*.csv | head -n1)',
-    dag = dag
+buatFile = PythonOperator(
+    task_id='buat_file',
+    python_callable = buat_file,
+    dag=dag
+)
+
+process = PythonOperator(
+    task_id='process',
+    python_callable = proses_data,
+    dag=dag
+)
+
+hapusFile = PythonOperator(
+    task_id='hapus_file',
+    python_callable = hapus_file,
+    dag=dag
 )
 
 done = BashOperator(
     task_id = 'done',
-    bash_command = 'echo "Done"',
+    bash_command = 'echo "Selesai"',
     dag = dag
 )
 
-# t1 = BashOperator(
-#     task_id = 'test',
-#     bash_command = 'ls /home/airflow/data/',
-#     dag = dag
-# )
-
-update_stock >> delete_file >> 
+buatTable >> buatFile >> process >> hapusFile >> done
